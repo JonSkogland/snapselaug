831ef89727f5d9ecfa830e4d018239606cc85d61
Jon Skogland <jon.skogland@gmail.com>
2025-09-14 14:44:00 +0200

    Flere ændring

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 # Så vi igang!
+
+# Funktion til oprettelse af snaps og snapseejer
+
+# Funktion til bedømmelse af snaps
+
+# Funktion til dashboard af snaps - og kunne se vinder
+
+# Funktion til at skrive til CSV fil
