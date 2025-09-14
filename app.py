040df7200f9989610f5286e81f8d6ab056be189a
Jon Skogland <jon.skogland@gmail.com>
2025-09-14 20:23:58 +0200

    Tilføje skrive til csv funktion

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
+# requirements
+import csv
+
 # opretter liste
-snapse=[]
+snapse = []
 # Funktion til oprettelse af snaps og snapseejer
 antal_spillere=int(input("hvor mange spillere er I?"))
 
 for i in range(antal_spillere):
-    navn=input("Skriv dit navn")
-    snapsenavn=input("Skriv navnet på din snaps")
-    snapse.append({"navn":navn,"snapsenavn":snapsenavn})
+    navn = input("Skriv dit navn")
+    snapsenavn = input("Skriv navnet på din snaps")
+    snapse.append({"navn":navn, "snapsenavn":snapsenavn})
     print(snapse)
 
 
 # Funktion til at skrive til CSV fil
-with open("snapse.txt","w") as f:
-    f.write()
+with open("snapse.csv", "w", newline="", encoding="utf-8") as csvfile:
+    key_navne = snapse[0].keys()
+    writer = csv.DictWriter(csvfile, fieldnames=key_navne)
+    writer.writeheader()
+    writer.writerows(snapse)
 
 # Funktion til bedømmelse af snaps
 
