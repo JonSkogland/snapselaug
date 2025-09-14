87362e95719a6d1db2778fc103e204d552554192
kimgraaversen-create <kim.graaversen@gmail.com>
2025-09-14 15:20:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-# Så vi igang!
-print("Hello world!")
+# opretter liste
+snapse=[]
 # Funktion til oprettelse af snaps og snapseejer
+antal_spillere=int(input("hvor mange spillere er I?"))
+
+for i in range(antal_spillere):
+    navn=input("Skriv dit navn")
+    snapsenavn=input("Skriv navnet på din snaps")
+    snapse.append({"navn":navn,"snapsenavn":snapsenavn})
+    print(snapse)
+
+
+# Funktion til at skrive til CSV fil
+with open("snapse.txt","w") as f:
+    f.write()
 
 # Funktion til bedømmelse af snaps
 
 # Funktion til dashboard af snaps - og kunne se vinder
 
-# Funktion til at skrive til CSV fil
